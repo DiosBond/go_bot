7ccfb7e9d9ac9a17821fe3fc135bb4d0c00919ab
denis bondar <Dios1986@ukr.net>
2024-05-06 11:30:52 +0300

    add exeptions and message handler

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -30,39 +31,50 @@ to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("kbot called", TeleToken)
 		kbot, err := telebot.NewBot(telebot.Settings{
-			URL: "",
-			Token: TeleToken,
-			Poller: &telebot.LongPoller{Timeout: 15 * time.Second}
-		
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 15 * time.Second},
+
 			// // Updates channel capacity, defaulted to 100.
 			// Updates int
-		
+
 			// // Poller is the provider of Updates.
 			// Poller Poller
-		
+
 			// // Synchronous prevents handlers from running in parallel.
 			// // It makes ProcessUpdate return after the handler is finished.
 			// Synchronous bool
-		
+
 			// // Verbose forces bot to log all upcoming requests.
 			// // Use for debugging purposes only.
 			// Verbose bool
-		
+
 			// // ParseMode used to set default parse mode of all sent messages.
 			// // It attaches to every send, edit or whatever method. You also
 			// // will be able to override the default mode by passing a new one.
 			// ParseMode ParseMode
-		
+
 			// // OnError is a callback function that will get called on errors
 			// // resulted from the handler. It is used as post-middleware function.
 			// // Notice that context can be nil.
 			// OnError func(error, Context)
-		
+
 			// // HTTP Client used to make requests to telegram api
 			// Client *http.Client
-		
+
 			// // Offline allows to create a bot without network for testing purposes.
 			// Offline bool
+		})
+
+		if err != nil {
+			log.Fatalf("Error: /n", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(mes telebot.Context) error {
+			log.Print(mes.Message().Payload, mes.Text())
+			return err
+
 		})
 	},
 }
